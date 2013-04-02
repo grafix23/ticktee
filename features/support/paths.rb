0821df47c019b8ea8fcb2e9806d681c05e87237a
Adam <grafix2@gate.net>
2013-04-02 14:15:25 -0400

    Creation of projects feature complete

---
+++ features/support/paths.rb
@@ -0,0 +1,15 @@
+module NavigationHelpers
+
+	def path_to(page_name)  
+	    case page_name
+	    when /the homepage/  
+	      root_path  
+	    when /the list of articles/  
+	      articles_path  
+	    else  
+	      raise "Can't find mapping from \"#{page_name}\" to a path."  
+	    end  
+	end
+end
+
+World(NavigationHelpers)
